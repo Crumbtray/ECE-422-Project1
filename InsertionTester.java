b29ef0fb7ed31c5a87780923f91d0f32b6df6589
Clinton Wong <clint.isaac.wong@gmail.com>
2013-10-16 23:09:10 -0600

    Loads the file now.

--- InsertionTester.java
+++ InsertionTester.java
@@ -1,6 +1,7 @@
 public class InsertionTester {
 	public static void main(String[] args)
 	{
+		System.loadLibrary("InsertionSort");
 		InsertionSort sorter = new InsertionSort();
 		int[] buffer = new int[]{0,1};
 		sorter.binarySort(buffer);
