c43d5ef7d079bce9effc7136ce463b2ae04194c2
Clinton Wong <clint.isaac.wong@gmail.com>
2013-10-17 09:57:07 -0600

    Tester checks the library path.

--- InsertionTester.java
+++ InsertionTester.java
@@ -1,6 +1,10 @@
+import java.io.*;
+import java.lang.*;
+
 public class InsertionTester {
 	public static void main(String[] args)
 	{
+		System.out.println("Java Library Path: " + System.getProperty("java.library.path"));
 		System.loadLibrary("InsertionSort");
 		InsertionSort sorter = new InsertionSort();
 		int[] buffer = new int[]{0,1};
